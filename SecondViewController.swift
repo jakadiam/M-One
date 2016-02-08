7c10dc23c74639593c475ed2ad92828c6a214580
jakadiam <watpiam2536@gmail.com>
2016-02-08 21:36:13 +0700

    first commit

---
+++ SecondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SecondViewController.swift
+//  M-One
+//
+//  Created by Jakadiam on 2/2/2559 BE.
+//  Copyright © 2559 Jakadiam. All rights reserved.
+//
+
+import UIKit
+
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
