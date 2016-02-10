1c21ceba38a0cc2f97524e951b22151410ab5af2
jakadiam <watpiam2536@gmail.com>
2016-02-10 21:03:36 +0700

    login-view

---
+++ M-One/ResultCell.swift
@@ -0,0 +1,26 @@
+//
+//  ResultCell.swift
+//  M-One
+//
+//  Created by Jakadiam on 2/9/2559 BE.
+//  Copyright © 2559 Jakadiam. All rights reserved.
+//
+
+import UIKit
+
+class ResultCell: UITableViewCell {
+
+    @IBOutlet weak var winView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
